35345cdf24a167ab5785ecb6c7b3c1dd77e95b02
Jens Burla Johansen <jens@burla.dk>
2020-04-18 20:38:56 +0200

    Civilian WIP

---
+++ LudumDare46/Assets/Scripts/HotspotManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HotspotManager : MonoBehaviour
+{
+    public List<GameObject> hotspots;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hotspots = new List<GameObject>(GameObject.FindGameObjectsWithTag("Hotspot"));
+    }
+}
