74b968be20780c90e4e3b6fc8a28f2ec42ac4c82
FLASHDENMARK <toffee@live.dk>
2020-04-20 00:38:36 +0200

    Sad face

---
+++ LudumDare46/Assets/Scripts/Interaction/BottleDamageable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottleDamageable : MonoBehaviour, IDamageReceiver
+{
+
+    public float health;
+    public float Health { get => health;}
+
+    public bool IsDead => throw new System.Exception();
+
+    public void Alert(ControllerBase damageGiver)
+    {
+        
+    }
+
+    public void Die(IDamageGiver damageGiver)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void ReceiveDamage(float damage, IDamageGiver damageGiver)
+    {
+        Destroy(this.gameObject);
+    }
+}
