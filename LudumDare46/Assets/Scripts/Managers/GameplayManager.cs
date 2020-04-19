8144af97c023a6eb6d59cb0bf8d3b2bad43cbd1c
FLASHDENMARK <toffee@live.dk>
2020-04-19 07:05:54 +0200

    Randomized civilian clothing

---
+++ LudumDare46/Assets/Scripts/Managers/GameplayManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Managers
+{
+    public class GameplayManager : MonoBehaviour
+    {
+        private void Awake()
+        {
+            
+        }
+    }
+}
