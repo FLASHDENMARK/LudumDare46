d813cf68506b0165ae4cf060969e471dd9608c16
Kasper Røgen <kasperhangard@hotmail.com>
2020-04-20 01:30:33 +0200

    Added lookattargetobjective & Fixed bottles

---
+++ LudumDare46/Assets/Scripts/AI/LookAtTargetObjective.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class LookAtTargetObjective : ObjectiveBase
+{
+    private bool _active;
+    public Material SniperMat;
+    public LineRenderer lineRenderer;
+    public override Vector3 GetBeginningVector()
+    {
+        _active = true;
+        return Vector3.zero;
+    }
+
+    public override Vector3 GetEndingVector()
+    {
+        _active = false;
+        return Vector3.zero;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRenderer.positionCount = 2;
+        lineRenderer.material = SniperMat;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1, _controller.transform.position);
+    }
+}
